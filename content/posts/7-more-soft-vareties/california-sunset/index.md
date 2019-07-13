a0dc3b398e2f064c462cf127eadba4e4afbac528
Jacob Simmerman <jgsimmerman@gmail.com>
2019-07-13 00:40:56 -0400

    Updated Catalog. Seems to work

--- content/posts/7-more-soft-vareties/california-sunset/index.md
+++ content/posts/7-more-soft-vareties/california-sunset/index.md
@@ -1,22 +1,22 @@
 ---
-path: "/catalog/aeonium/kiwi"
-cover: "./kiwi.jpg"
-pic: "./kiwi.jpg"
-title: "Kiki"
-scientificname: "Aeonium"
-id: 8
-catalog-section: "aeonium"
-tags: ['aeonium']
-primarycolor: "Green"
-stresscolors: "Pink"
+path: "/catalog/more-soft-varieties/california-sunset"
+cover: "./succ3.jpg"
+pic: "./succ3.jpg"
+title: "California Sunset"
+scientificname: "Graptosedum"
+id: 7
+catalog-section: "more-soft-varieties"
+tags: ['More Soft Varieties']
+primarycolor: ""
+stresscolors: ""
 bloomcolor: ""
 petsafe: ""
 temperature: ""
 seasonality: ""
-inventory: 14
-price: 4.25
+inventory: 30
+price: 3.95
 published: true
 story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 ---
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
--- src/pages/catalog/more-soft-varieties.jsx
+++ src/pages/catalog/more-soft-varieties.jsx
@@ -21,7 +21,7 @@ const CatalogWrapper = styled.div`
   }
 `;
 
-const Aeonium = ({ data }) => {
+const MoreSoftVarieties = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
   return (
     <Layout>
@@ -32,7 +32,7 @@ const Aeonium = ({ data }) => {
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
       <script id="snipcart" src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="YjdiNWIyOTUtZTIyMy00MWMwLTkwNDUtMzI1M2M2NTgxYjE0"></script>
       </Helmet> */}
-      <Header title="Aeonium">Sweet Leaf Succulents and Ornamental Plants</Header>
+      <Header title="More Soft Varieties">Sweet Leaf Succulents and Ornamental Plants</Header>
       <CatalogWrapper>
       {edges.map(({ node }) => (
         <BlogList
@@ -50,9 +50,9 @@ const Aeonium = ({ data }) => {
   );
 };
 
-export default Aeonium;
+export default MoreSoftVarieties;
 
-Aeonium.propTypes = {
+MoreSoftVarieties.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.arrayOf(
@@ -75,7 +75,7 @@ Aeonium.propTypes = {
 
 export const query = graphql`
   query {
-    allMarkdownRemark(filter: {frontmatter: {id: {eq: 8}}}) {
+    allMarkdownRemark(filter: {frontmatter: {id: {eq: 7}}}) {
       edges {
         node {
           id
