b4010c0223baa6020e63bc25295f28d5ed0aa4d1
Jacob Simmerman <jgsimmerman@gmail.com>
2019-07-13 00:32:22 -0400

    Partial catalog update

---
+++ src/pages/catalog/aeonium.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import { Header, BlogList } from 'components';
+import { Layout } from 'layouts';
+
+
+const CatalogWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 4rem 4rem 1rem 4rem;
+  @media (max-width: 1000px) {
+    margin: 4rem 2rem 1rem 2rem;
+  }
+  @media (max-width: 700px) {
+    margin: 4rem 1rem 1rem 1rem;
+  }
+`;
+
+const Aeonium = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+  return (
+    <Layout>
+      {/*<Helmet title={'Catalog'} /> */}
+      {/* <Helmet htmlAttributes={{ lang: 'en' }}>
+      <title>{'Catalog'}</title>
+      <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+      <script id="snipcart" src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="YjdiNWIyOTUtZTIyMy00MWMwLTkwNDUtMzI1M2M2NTgxYjE0"></script>
+      </Helmet> */}
+      <Header title="Aeonium">Sweet Leaf Succulents and Ornamental Plants</Header>
+      <CatalogWrapper>
+      {edges.map(({ node }) => (
+        <BlogList
+          key={node.id}
+          cover={node.frontmatter.cover.childImageSharp.fluid}
+          path={node.frontmatter.path}
+          title={node.frontmatter.title}
+          date={node.frontmatter.date}
+          tags={node.frontmatter.tags}
+          excerpt={node.excerpt}
+        />
+      ))}
+      </CatalogWrapper>
+    </Layout>
+  );
+};
+
+export default Aeonium;
+
+Aeonium.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            excerpt: PropTypes.string,
+            frontmatter: PropTypes.shape({
+              cover: PropTypes.object.isRequired,
+              path: PropTypes.string.isRequired,
+              title: PropTypes.string.isRequired,
+              date: PropTypes.string.isRequired,
+              tags: PropTypes.array,
+            }),
+          }),
+        }).isRequired
+      ),
+    }),
+  }),
+};
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(filter: {frontmatter: {id: {eq: 4}}}) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 200)
+          frontmatter {
+            title
+            path
+            tags
+            id
+            price
+            date(formatString: "MM.DD.YYYY")
+            cover {
+              childImageSharp {
+                fluid(
+                  maxWidth: 1000
+                  quality: 90
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
