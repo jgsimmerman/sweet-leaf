12ad31170da2f7ceec71943cc215ea2fb569f4bb
Jacob Simmerman <jgsimmerman@gmail.com>
2019-11-17 02:09:31 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,8 @@ module.exports = {
       resolve: 'gatsby-plugin-snipcart',
       options: {
           //jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+          js: 'https://cdn.snipcart.com/themes/v3.0.0/default/snipcart.js',
+          styles: 'https://cdn.snipcart.com/themes/v3.0.0/default/snipcart.css',
           apiKey: 'OWRmMjdlZWUtNzM1NS00YmQzLWFlN2EtOGU2MTIyOGQyZDQ4NjM2OTUwMjk5ODUzNDc5OTgw',
           autopop: true,
       }
