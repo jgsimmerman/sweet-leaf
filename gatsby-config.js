cf938421068100c5af9e75245173d8890c524788
Jacob <jgsimmerman@gmail.com>
2019-11-24 23:16:56 -0500

    Added purgecss plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,5 +99,16 @@ module.exports = {
           credentials: require('./succulents-eceeb0db1c4f.json')
       }
     }, */
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    }
   ],
 };
