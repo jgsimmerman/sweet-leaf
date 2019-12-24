cc1509bf93d64770bab82338540e9c8095d2f3e6
Jacob <jgsimmerman@gmail.com>
2019-12-24 01:30:34 -0500

    Plugin work...

--- src/components/ZygoteCart.jsx
+++ src/components/ZygoteCart.jsx
@@ -42,7 +42,7 @@ const ZygoteCart = props => {
       //stripeApiKey=`${process.env.STRIPE_API_PUBLIC}`
       infoWebhook='/.netlify/functions/info-stripe'
 			orderWebhook='/.netlify/functions/order-stripe'
-      plugins={[payment]}
+      plugins={[ payment ]}
 
       cartHeader={<div>Sweet Leaf Succulents</div>}
 
