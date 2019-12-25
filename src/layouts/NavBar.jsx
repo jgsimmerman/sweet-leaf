a1fd1b1f6b60482d1c5e2538cee9606f6bbe6d9b
Jacob <jgsimmerman@gmail.com>
2019-12-25 00:11:39 -0500

    Cleaned up jsx

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -73,7 +73,6 @@ const NavBar = (props) => (
             )}
           </CartQuantity>
           {/* |<span className="snipcart-total-price"></span> */}
-        </a>
       </div>
     </Nav>
   </Headroom>
