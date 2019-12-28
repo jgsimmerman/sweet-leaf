9570daf1274e848be7d3cb5d36da74cad02faa8c
Jacob <jgsimmerman@gmail.com>
2019-12-28 08:26:48 -0500

    update cart

--- src/functions/submit-stripe-info.js
+++ src/functions/submit-stripe-info.js
@@ -135,7 +135,7 @@ export default async function submitStripeInfo({ stripeApiSecret, body, verbose 
 		})
 	}
 
-
+	
 	// Get shipping
 	if (order.shipping_methods) {
 		res.shippingMethods = order.shipping_methods.map(({ id, amount, description }) => {
