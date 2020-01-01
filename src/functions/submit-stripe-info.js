b64c2b143e4efcdf5f47c83a06a6d74c60797b62
Jacob <jgsimmerman@gmail.com>
2020-01-01 12:03:46 -0500

    changed strip.order object

--- src/functions/submit-stripe-info.js
+++ src/functions/submit-stripe-info.js
@@ -63,7 +63,17 @@ export default async function submitStripeInfo({ stripeApiSecret, body, verbose 
 					country: `US`,
 				},
 			},
-			
+			shipping_methods: [
+				{
+					id: `ship0`,
+				},
+				{
+					id: `ship1`,
+				},
+				{
+					id: `ship2`,
+				},
+			]
 			
 		}
 		if (body.coupon) {
