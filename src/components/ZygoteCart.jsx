9768f925a80da418321d266b7f954c7e6b49a633
Jacob <jgsimmerman@gmail.com>
2020-01-02 17:22:22 -0500

    plugin

--- src/components/ZygoteCart.jsx
+++ src/components/ZygoteCart.jsx
@@ -16,7 +16,7 @@ import { Cart } from 'react-cart-test';
 // import * as calculatetax from "calculatetax";
 // import * as secondplugin from "secondplugin";
 // import * as componentTest from "componentTest";
-//import getShippingMethods from "./getShippingMethods";
+import getShippingMethods from "./getShippingMethods";
 
 //import * as EscaAPI from '@escaladesports/zygote-plugin-esca-api';
 
@@ -43,9 +43,9 @@ const ZygoteCart = props => {
       infoWebhook='/.netlify/functions/info-stripe'
 			orderWebhook='/.netlify/functions/order-stripe'
       
-      // plugins={[ 
-      //   getShippingMethods 
-      // ]}
+      plugins={[ 
+        getShippingMethods 
+      ]}
 
       // cartHeader={<div>Sweet Leaf Succulents</div>}
 
