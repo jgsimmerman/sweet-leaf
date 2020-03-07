5fff82f5b8e4223d7a7bf6f70cd5b4ea55ac124d
Jacob <jgsimmerman@gmail.com>
2020-03-06 23:26:41 -0500

    updates

--- src/components/ZygoteCart.jsx
+++ src/components/ZygoteCart.jsx
@@ -32,7 +32,7 @@ const ZygoteCart = props => {
     <Cart
      //stripeApiKey="pk_live_nMctV4G1movcajzQsjXet2Zs000zybvAUV"
      stripeApiKey="pk_test_Vc8z3p2pdxHFQgxhbbhIXtyv00GnPddsjV"
-
+    
       //stripeApiKey=`${process.env.STRIPE_API_PUBLIC}`
       infoWebhook='/.netlify/functions/info-stripe'
       orderWebhook='/.netlify/functions/order-stripe'
